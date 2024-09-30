5dea598403ab91d482c9b7a234807f9fbbb6f358
tosin <akinsemoyintosin1@gmail.com>
2024-09-30 18:26:00 +0100

    add new projects to my project section

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -7,7 +7,6 @@ import { useSectionInView } from "@/lib/hooks";
 export default function AbouMe() {
   const { ref } = useSectionInView("About");
 
-
   // const { ref, inView } = useInView({
   //   threshold: 0.75,
   // });
@@ -29,7 +28,48 @@ export default function AbouMe() {
       className="mb-28 max-w-[45rem] text-center leading-8 scroll-mt-28"
     >
       <SectionHeading>About Me</SectionHeading>
-      <p className="mb-3">
+      <p>
+        I graduated from the University of Lagos with a degree in{" "}
+        <span className="font-medium">Insurance </span> and am now a dedicated
+        <span className="font-medium">Frontend Developer</span> . I am currenty
+        a <span className="font-medium">frontend web developer</span> intern at
+        Zulfah Academy, where I actively participate in hands-on projects,
+        coding exercises, and collaborative activities using{" "}
+        <span className="font-medium">HTML</span>,{" "}
+        <span className="font-medium">CSS,</span>{" "}
+        <span className="font-medium">JavaScript,</span> and{" "}
+        <span className="font-medium">React</span> to further enhance my
+        practical skills and understanding of frontend development. I specialize
+        in building modern, responsive web applications using technologies like
+        <span className="font-medium">React JS</span> ,{" "}
+        <span className="font-medium">Next.js</span> , and{" "}
+        <span className="font-medium">Tailwind CSS</span> , with projects
+        ranging from an e-commerce app featuring product filtering and cart
+        functionality to a weather app displaying real-time data and more. I am
+        passionate about solving problems through clean, efficient code and
+        design, and I’m always open to new opportunities to collaborate and
+        create impactful solutions
+      </p>
+      <p>
+        <span className="italic">Beyond Coding coding</span>, I have diverse
+        interests in sports, design, cooking, nature, and landscape photography.
+        I also enjoy <span className="font-medium">learning new things</span>. I
+        am currently learning about{" "}
+        <span className="font-medium">UI design </span>. I thrive in
+        environments that foster creativity and continuous learning.
+      </p>
+    </motion.section>
+  );
+}
+
+/*
+I graduated from the University of Lagos, Nigeria, with a degree in insurance. My passion for programming led me to complete a rigorous six-month Front-End Web Development program at Zulfah Academy. This experience taught me proficiency in HTML, CSS, JavaScript, and React. I am deeply motivated by problem-solving and enjoy providing innovative solutions to challenges individuals and businesses face.
+With a core stack in React and Next.js, supplemented by familiarity with TypeScript, I seek a full-time position as a junior software developer. I am eager to apply my skills and continue my growth in the field.
+Beyond coding, I have diverse interests in sports, design, cooking, nature, and landscape photography. I thrive in environments that foster creativity and continuous learning.
+
+*/
+/*
+<p className="mb-3">
         I graduated from the University of Lagos, Nigeria, with a degree{" "}
         <span className="font-medium">Insurance</span>, My passion for
         programming led me to complete a rigorous six-month{" "}
@@ -50,22 +90,5 @@ export default function AbouMe() {
         <span className="font-medium">full-time position</span> as a junior
         front-end developer.
       </p>
-
-      <p>
-        <span className="italic">Beyond Coding coding</span>, I have diverse
-        interests in sports, design, cooking, nature, and landscape photography.
-        I also enjoy <span className="font-medium">learning new things</span>. I
-        am currently learning about{" "}
-        <span className="font-medium">UI design </span>. I thrive in
-        environments that foster creativity and continuous learning.
-      </p>
-    </motion.section>
-  );
-}
-
-/*
-I graduated from the University of Lagos, Nigeria, with a degree in insurance. My passion for programming led me to complete a rigorous six-month Front-End Web Development program at Zulfah Academy. This experience taught me proficiency in HTML, CSS, JavaScript, and React. I am deeply motivated by problem-solving and enjoy providing innovative solutions to challenges individuals and businesses face.
-With a core stack in React and Next.js, supplemented by familiarity with TypeScript, I seek a full-time position as a junior software developer. I am eager to apply my skills and continue my growth in the field.
-Beyond coding, I have diverse interests in sports, design, cooking, nature, and landscape photography. I thrive in environments that foster creativity and continuous learning.
 
 */
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,6 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import CryptoWebsiteImg from "@/public/Crypto-website.png";
-import DataWebsite from "@/public/Data-web site.png";
-import NetflixImg from "@/public/NeflixClone-landing.png";
 import modernWebsite from "@/public/modern-website.png";
 import cssImg from "@/public/skills/css.png";
 import githubImg from "@/public/skills/github1.png";
@@ -13,6 +10,10 @@ import javascriptImg from "@/public/skills/javascript.png";
 import nextImg from "@/public/skills/nextjs.png";
 import reactImg from "@/public/skills/react.png";
 import tailwingImg from "@/public/skills/tailwind.png";
+import ecommerWebsite from "@/public/ecommerce-website.png";
+import calculatorTwo from "@/public/calculatorTwo.png";
+import weatherApp from "@/public/weatherApp.png";
+import sushiWebsite from "@/public/sushi-website.png"
 
 export const links = [
   {
@@ -37,32 +38,41 @@ export const links = [
   },
 ] as const;
 
-
 export const projectsData = [
   {
-    title: "Crypto Currency Wesite",
+    title: "Ecommerce Website",
     description:
-      "I built a dynamic Cryptocurrency website utilizing modern web development technologies, including React Hooks, Context API, React Router Dom, useParams, and Tailwind CSS.",
-    tags: ["React", "Firebase", "Tailwind"],
-    imageUrl: CryptoWebsiteImg,
+      "I built a dynamic Ecommerce website utilizing modern web development technologies, including React Hooks, Context API, React Router Dom, useParams, and Tailwind CSS.",
+    tags: ["React", "Toastifyg", "Tailwind"],
+    imageUrl: ecommerWebsite,
+  },
+
+  {
+    title: "Calculator App",
+    description: `Built a simple functional calculator using HTML, CSS, and JavaScript. Implemented basic arithmetic operations like addition, subtraction, multiplication, and division. Used JavaScript DOM manipulation to handle user inputs and display results dynamically.`,
+    tags: ["HTML", "CSS", "JavaScrit"],
+    imageUrl: calculatorTwo,
   },
   {
-    title: "Data website",
-    description: `Developed a data centric website using React JS and Tailwind CSS, ensuring 100% mobile responsive from scratch Implemented engaging text animations in the Hero component utilizing the "React-Typed" package.`,
-    tags: ["React", "Tailwind"],
-    imageUrl: DataWebsite,
-  },
-  {
-    title: "Netflix Clone Landing page",
-    description: `Developed a responsive and visually appealing clone of the Netflix landing page using HTML and CSS.  Ensured a mobile-friendly design for a seamless user experience on mobile devices. `,
-    tags: ["Html", "CSS"],
-    imageUrl: NetflixImg,
+    title: "Weather App",
+    description: `Developed a responsive weather application using HTML, CSS, JavaScript, 
+  Integrated the OpenWeather API to fetch and display real-time weather information.
+  Implemented dynamic UI updates based on user input and weather data.
+  Improved proficiency in API integration and handling asynchronous JavaScript operations `,
+    tags: ["Html", "CSS", "JavaScript", "OpenWeather API"],
+    imageUrl: weatherApp,
   },
   {
     title: "Fintech Website",
-    description: `I created a modern Fintech Responsive landing page with React JS, featuring a captivating hero section, high-quality assets, dynamic gradients, business statistics, and impactful testimonials.`,
+    description: `I created a modern Static Responsive landing page with React JS, featuring a captivating hero section, high-quality assets, dynamic gradients, business statistics, and impactful testimonials.`,
     tags: ["React", "Tailwind"],
     imageUrl: modernWebsite,
+  },
+  {
+    title: "Sushi Website ",
+    description: `Designed and developed a visually appealing fully responsive , static sushi-themed website using HTML and CSS, leveraging Vite for fast builds and optimized performance. Incorporated AOS (Animate On Scroll) library for smooth scroll animations, enhancing user experience`,
+    tags: ["HTML", "CSS", "VITE"],
+    imageUrl: sushiWebsite,
   },
 ] as const;
 
